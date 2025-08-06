82260149a4adad2cce96a25ce1b5f7b6fbe4501a
yookgaejang11 <yookprograming@gmail.com>
2025-08-06 21:10:00 +0900

    게임매니저

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public int width = 10;
+    public int height = 10;
+    public bool[,] occupied;    //true = 타일에 건물 있음
+    public bool isBuilding = false; //건물을 지을 수 있는 상태
+
+    public int[,] map = new int[,]
+    {
+        { 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0 },
+    };
+
+    private void Awake()
+    {
+        Instance = this;
+        width = map.GetLength(0);
+        height = map.GetLength(1);
+        occupied = new bool[width, height];
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public bool IsAreaFree(Vector2Int start, Vector2Int size)
+    {
+        for(int x = 0; x < size.x; x++)
+        {
+            for(int y = 0; y < size.y; y++)
+            {
+                int checkX = start.x + x;
+                int checkY = start.y + y;
+                if (checkX < 0 || checkY < 0 || checkX >= width || checkY >= height)
+                {
+                    return false;
+                }
+                if(occupied[checkX, checkY])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    public void OccupyArea(Vector2Int start, Vector2Int size)
+    {
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+                occupied[start.x + x, start.y + y] = true;
+            }
+        }
+    }
+
+}
